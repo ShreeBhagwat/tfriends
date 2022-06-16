df53a3a2e16d6b160c231f8a9a8779e8d115bd58
Aniket Kharat <aniket@zensung.com>
2022-06-16 18:18:11 +0530

    Third Time Edited Masters Branch

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class TFriends extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Aniket Kharat',
+      title: 'New Chnages In Masters',
       home: LoginScreen(),
     );
   }
