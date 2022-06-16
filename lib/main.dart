08469d0d029006f3f6593b378ebd3e77880473f7
Aniket Kharat <aniket@zensung.com>
2022-06-16 18:19:52 +0530

    Changes In Aniket Branch

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class TFriends extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'New New Changes Came',
       home: LoginScreen(),
     );
   }
