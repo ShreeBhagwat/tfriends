8ced1d052a2b2b62d0632471505bc0bfa51349b9
Shree Bhagwat <shreebhagwat94@gmail.com>
2022-06-18 10:08:03 +0530

    Extracted loader widget

--- lib/Screens/login_screen.dart
+++ lib/Screens/login_screen.dart
@@ -155,43 +155,7 @@ class LoginScreen extends StatelessWidget {
                     ),
                   ],
                 ),
-                userInfoManage.isLoading
-                    ? Container(
-                        width: MediaQuery.of(context).size.width,
-                        height: MediaQuery.of(context).size.height,
-                        color: Colors.transparent,
-                        child: Center(
-                            child: Container(
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10),
-                            color: Colors.grey.withOpacity(0.5),
-                          ),
-                          width: 100,
-                          height: 100,
-                          child: Container(
-                            width: 80,
-                            height: 80,
-                            child: Center(
-                              child: Column(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceEvenly,
-                                children: [
-                                  CircularProgressIndicator(
-                                    color: Colors.white,
-                                  ),
-                                  Text(
-                                    'Loading ...',
-                                    style: TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 15,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ),
-                        )))
-                    : Container()
+                  CutomLoader(isLoading: userInfoManage.isLoading, loadingText: 'Loading',),
               ],
             ),
           ),
@@ -209,5 +173,56 @@ class LoginScreen extends StatelessWidget {
     //       builder: (_) => DashboardScreen(),
     //     ),
     //     (route) => false);
+  }
+}
+
+class CutomLoader extends StatelessWidget {
+  const CutomLoader({
+    Key? key,
+    required this.isLoading,
+    required this.loadingText,
+  }) : super(key: key);
+
+  final bool isLoading;
+  final String loadingText;
+
+  @override
+  Widget build(BuildContext context) {
+    return isLoading
+        ? Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            color: Colors.transparent,
+            child: Center(
+                child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.grey.withOpacity(0.5),
+              ),
+              width: 100,
+              height: 100,
+              child: Container(
+                width: 80,
+                height: 80,
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      CircularProgressIndicator(
+                        color: Colors.white,
+                      ),
+                      Text(
+                        loadingText,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 15,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            )))
+        : Container();
   }
 }
